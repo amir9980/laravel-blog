ca693689490ff482f1f9281f01525b2c27754df5
amir-abbas ramezanipoor <amir.9980@yahoo.com>
2022-07-27 10:24:38 +0430

    migrations and seeders fixed

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'title' => 'user'
+            ],
+            [
+                'title' => 'writer'
+            ],
+            [
+                'title' => 'admin'
+            ]
+        ];
+
+        Role::insert($data);
+    }
+}
