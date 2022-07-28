53f2e0344d65af3e7677a8f236b75193bf5aae91
imalireza.py <mousaviali1381@gmail.com>
2022-07-28 08:26:06 -0700

    like and bookmark finished

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,21 +1,21 @@
 function bookmark(item) {
-    if (item.className === "fa-thin fa-bookmark articles-bookmark") {
-        item.className = "fa fa-bookmark articles-bookmark";
+    if (item.className === "  fa-thin fa-bookmark articles-bookmark ") {
+        item.className = "  fa fa-bookmark articles-bookmark ";
         apply(item.id, 'bookmark', `post`)
     } else {
-        item.className = "fa-thin fa-bookmark articles-bookmark";
+        item.className = "  fa-thin fa-bookmark articles-bookmark ";
         apply(item.id, 'bookmark', `delete`)
 
     }
 }
 
 function heart(item) {
-    if (item.className === "fa-thin fa-heart articles-bookmark") {
-        item.className = "fa fa-heart articles-bookmark";
+    if (item.className === "  fa-thin fa-heart articles-bookmark ") {
+        item.className = "  fa fa-heart articles-bookmark ";
         apply(item.id, 'like', `post`)
 
     } else {
-        item.className = "fa-thin fa-heart articles-bookmark";
+        item.className = "  fa-thin fa-heart articles-bookmark ";
         apply(item.id, 'like', `delete`)
 
     }
