5951fa9a7d41a734dc23d610032f971d692c19d1
amir-abbas ramezanipoor <amir.9980@yahoo.com>
2022-07-30 16:23:48 +0430

    commit

--- resources/views/main/layouts/master.blade.php
+++ resources/views/main/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
 
     <title>@yield("title")</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
