c5f0ae90109c458b1ed1c54d1b5114e3b90d9312
imalireza.py <mousaviali1381@gmail.com>
2022-08-01 23:12:23 -0700

    fix ckeditor upload image

--- resources/views/main/article_create.blade.php
+++ resources/views/main/article_create.blade.php
@@ -1,7 +1,7 @@
 @extends('main.layouts.master')
 @section('title', 'ایجاد مقاله')
 @section('link')
-    <script src="/js/ckeditor/ckeditor.js"></script>
+    <script src="/js/ckeditor-s/ckeditor.js"></script>
 @endsection
 @section('content')
     @include('main.layouts.includes.errors')
@@ -82,6 +82,100 @@
         }
     </script>
     <script>
-        CKEDITOR.replace( 'body',{filebrowserImageBrowseUrl: '/file-manager/ckeditor'});
+        {{--// import {ClassicEditor} from "../../../public/js/ckeditor5/ckeditor";--}}
+
+        {{--ClassicEditor--}}
+        {{--    .create(document.getElementById('body'),{--}}
+        {{--        ckfinder:{--}}
+        {{--            uploadUrl: '{{route('ck.upload').'?_token='.csrf_token()}}',--}}
+        {{--        },--}}
+        {{--    })--}}
+        {{--    .then(editor => {--}}
+        {{--        console.log(editor);--}}
+        {{--    })--}}
+        {{--    .catch(error => {--}}
+        {{--        console.error(error);--}}
+        {{--    });--}}
+
+        CKEDITOR.addCss('figure[class*=easyimage-gradient]::before { content: ""; position: absolute; top: 0; bottom: 0; left: 0; right: 0; }' +
+            'figure[class*=easyimage-gradient] figcaption { position: relative; z-index: 2; }' +
+            '.easyimage-gradient-1::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 66, 174, 234, .72 ) 100% ); }' +
+            '.easyimage-gradient-2::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 228, 66, 234, .72 ) 100% ); }');
+
+        CKEDITOR.replace('body', {
+            extraPlugins: 'easyimage',
+            removePlugins: 'image',
+            removeDialogTabs: 'link:advanced',
+            toolbar: [{
+                name: 'document',
+                items: ['Undo', 'Redo']
+            },
+                {
+                    name: 'styles',
+                    items: ['Format']
+                },
+                {
+                    name: 'basicstyles',
+                    items: ['Bold', 'Italic', 'Strike','Rename', '-', 'RemoveFormat']
+                },
+                {
+                    name: 'paragraph',
+                    items: ['NumberedList', 'BulletedList']
+                },
+                {
+                    name: 'links',
+                    items: ['Link', 'Unlink']
+                },
+                {
+                    name: 'insert',
+                    items: ['EasyImageUpload']
+                }
+            ],
+            height: 630,
+            // uploadUrl: '/uploads/imgs',
+            cloudServices_uploadUrl: 'https://33333.cke-cs.com/easyimage/upload/',
+            // Note: this is a token endpoint to be used for CKEditor 4 samples only. Images uploaded using this token may be deleted automatically at any moment.
+            // To create your own token URL please visit https://ckeditor.com/ckeditor-cloud-services/.
+            cloudServices_tokenUrl: 'https://33333.cke-cs.com/token/dev/ijrDsqFix838Gh3wGO3F77FSW94BwcLXprJ4APSp3XQ26xsUHTi0jcb1hoBt',
+
+            easyimage_styles: {
+                gradient1: {
+                    group: 'easyimage-gradients',
+                    attributes: {
+                        'class': 'easyimage-gradient-1'
+                    },
+                    label: 'Blue Gradient',
+                    icon: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/gradient1.png',
+                    iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/hidpi/gradient1.png'
+                },
+                gradient2: {
+                    group: 'easyimage-gradients',
+                    attributes: {
+                        'class': 'easyimage-gradient-2'
+                    },
+                    label: 'Pink Gradient',
+                    icon: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/gradient2.png',
+                    iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/hidpi/gradient2.png'
+                },
+                noGradient: {
+                    group: 'easyimage-gradients',
+                    attributes: {
+                        'class': 'easyimage-no-gradient'
+                    },
+                    label: 'No Gradient',
+                    icon: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/nogradient.png',
+                    iconHiDpi: 'https://ckeditor.com/docs/ckeditor4/4.19.1/examples/assets/easyimage/icons/hidpi/nogradient.png'
+                }
+            },
+            easyimage_toolbar: [
+                'EasyImageFull',
+                'EasyImageSide',
+                'EasyImageGradient1',
+                'EasyImageGradient2',
+                'EasyImageNoGradient',
+                'EasyImageAlt'
+            ],
+            removeButtons: 'PasteFromWord'
+        });
     </script>
 @endsection
